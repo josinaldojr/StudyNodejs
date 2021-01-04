cc0321819ee8a9f5f8dca7280be463e3da075874
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-01-04 10:44:18 -0300

    feat(#3): create controller and routes for api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const http = require('http');
 
-const users = require('./mocks/users');
+const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
 
-  if(request.url === '/users' && request.method === 'GET') {
-    response.writeHead(200, { 'Content-Type': 'aplication/json'});
-    response.end(JSON.stringify(users))
+  const route = routes.find((routeObj) => (
+    routeObj.endpoint === request.url && routeObj.method === request.method
+  ));
+
+  if(route) {
+    route.handler(request, response);
   } else {
-    response.writeHead(200, { 'Content-Type': 'text/html'});
+    response.writeHead(404, { 'Content-Type': 'text/html'});
     response.end(`Cannot ${request.method} ${request.url}`)
   }
 })
